f7a048cabc5d946ebabe40ab1a28a031385f809c
Peter Tuan Anh <96538547+PeterTADev@users.noreply.github.com>
2022-02-08 22:14:15 +0700

    Update events.js

--- src/events.js
+++ src/events.js
@@ -20,7 +20,7 @@ player.on('botDisconnect', (queue) => {
 });
 
 player.on('channelEmpty', (queue) => {
-    queue.metadata.send('Nobody is in the voice channel, leaving the voice channel... ❌');
+    queue.metadata.send('Nobody is in the voice channel, leaving the voice channel.. ❌');
 });
 
 player.on('queueEnd', (queue) => {
